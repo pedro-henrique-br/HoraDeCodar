0bbe125732de0229d863bf84aadcae8f35e6759b
Pedro Henrique <doctorfast10.12@hotmail.com>
2023-10-17 06:28:11 -0300

    JS

---
+++ JS/1-TiposDeDadosEOperadores/script.js
@@ -0,0 +1,29 @@
+// Numer
+
+const numero = 10;
+
+// String
+
+const fruta = "Laranja";
+
+// Boolean
+
+const booleano = false;
+
+// Empty values 
+
+const nulo = null
+
+// Objeto 
+
+const objeto = {
+  Escrivaninha : "preta",
+  escada : "prata"
+}
+
+console.log(objeto)
+console.log(booleano)
+console.log(numero)
+console.log(fruta)  
+console.log(nulo)  
+
---
+++ JS/2-EstruturasDeProgamação/script.js
@@ -0,0 +1,57 @@
+
+// Prompt e Alert
+
+// const caixaDeAlerta = prompt("Digite seu nome")
+// const CaixaDeMsg = alert(`Seu nome é ${caixaDeAlerta}`)
+
+// console.log(Math.min(2, 4, 1, 9, 0.2))
+// console.log(Math.floor(2.73))
+// console.log(Math.ceil(4.89))
+
+// If - Else
+
+// const poupanca = 1214.00
+// const salario = 1217.23
+
+// if (poupanca > salario) {
+//   console.log("salario é maior que a sua poupança")
+// }
+// else {
+//   console.log("sua poupança é maior que o salario")
+// }
+
+// if (salario < poupanca) {
+//   console.log("!")
+// }
+// else if (salario <= poupanca) {
+//   console.log("vish")
+// }
+// else {
+//   console.log("deu bom")
+// }
+
+// While
+
+// let numb = 0;
+
+// while( numb <= 10) {
+//   console.log(numb)
+//   numb++
+// }
+
+
+// Do While
+
+// let numb = 0;
+
+// do {
+//   console.log(numb)
+//   numb++
+// } while (numb <= 10)
+
+
+// For
+
+// for(numb = 0; numb <= 10; numb++){
+//   console.log(numb)
+// }
